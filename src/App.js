38d91cd9779aa78a8ff342cf65eb9c14cef6fd88
Will brown <wtbrown10@gmail.com>
2020-10-08 21:23:12 -0400

    nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
 import './App.css';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
     </div>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NavBar() {
     return (
         <div>
-            
+            <h1>This is the NavBar</h1>
         </div>
     )
 }
